22d40c88cc02e3cf2ea01108a7458de959042c62
Ahmed Gomaa <ahmedgomaa122003@gmail.com>
2026-04-26 01:02:34 +0300

    setup project and gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const mongoose = require("mongoose");
-// const uri = 'mongodb+srv://ahmed:node_123@learn-mongo-db.o7bpiq4.mongodb.net/?appName=learn-mongo-DB';
+const uri_DB = process.env.uri_DB;
 
-const uri = 'mongodb+srv://ahmed:ahmed_123@learn-mongo-db.o7bpiq4.mongodb.net/coursesCRUD?retryWrites=true&w=majority';
-mongoose.connect(uri).then(() => console.log("connected to MongoDB"));
+
+mongoose.connect(uri_DB).then(() => console.log("connected to MongoDB"));
 
 const app = express();
 app.use(express.json());
